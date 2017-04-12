25601baa2894256798096341868ffa314a64972c
Paul Oldham <poldham79@gmail.com>
2017-04-12 15:38:53 -0300

    words

--- ipCheck.sh
+++ ipCheck.sh
@@ -2,6 +2,6 @@
 CURRENT_IP=$(curl -s 'https://api.ipify.org')
 if [[ "${CURRENT_IP}" != $(cat ~/.myIp) ]]
 then
-	$(sendemail -q -l email.log -f "emailfor@froml.field" -u "**IP CHANGED**subject" -t "emailtosend@notification.to" -s "smtp.gmail.com:587" -o "tls=yes" -xu "yourusername@gmail.com" -xp "yourpassword" -m "New IP ==  $CURRENT_IP.  \nBody of email - I use a link to my dns provider here")
+	$(sendemail -q -l email.log -f "emailfor@from.field" -u "**IP CHANGED**subject" -t "emailtosend@notification.to" -s "smtp.gmail.com:587" -o "tls=yes" -xu "yourusername@gmail.com" -xp "yourgmailpassword" -m "New IP ==  $CURRENT_IP.  \nBody of email - I use a link to my dns provider here")
 $(echo $CURRENT_IP > .myIp)
 fi
